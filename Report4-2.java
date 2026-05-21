dfd29cc2a1ae3730820664e2e89c4182e5008b50
Mutsuki <cod14016@ict.nitech.ac.jp>
2026-05-21 15:00:49 +0900

    Update Report4-2 on feature branch

--- Report4-2.java
+++ Report4-2.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 // shortest path algorithm
+// branch update
 class FlowerNumber{
     public static void main(String[] args){
         Scanner stdIn = new Scanner(System.in);
