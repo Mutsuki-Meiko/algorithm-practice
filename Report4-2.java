05d2087ced8471ded03b411322b1d0e729c4b05c
Mutsuki <cod14016@ict.nitech.ac.jp>
2026-05-21 14:57:00 +0900

    Add Report4-2 shortest path implementation

---
+++ Report4-2.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+// shortest path algorithm
+class FlowerNumber{
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("数字を入力してください。");
+        int n = stdIn.nextInt();
+
+        String nS = Integer.toString(n);
+
+        for(int i = 0; i < nS.length(); i++){
+            char digitChar = nS.charAt(i);
+            int digit = Character.getNumericValue(digitChar);
+            switch (digit) {
+                case 0:
+                    System.out.println("  ***  ");
+                    System.out.println(" *   * ");
+                    System.out.println(" *   * ");
+                    System.out.println(" *   * ");
+                    System.out.println(" *   * ");
+                    System.out.println(" *   * ");
+                    System.out.println("  ***  ");
+                    break;
+                case 1:
+                    System.out.println("   *   ");
+                    System.out.println("  **   ");
+                    System.out.println(" * *   ");
+                    System.out.println("   *   ");
+                    System.out.println("   *   ");
+                    System.out.println("   *   ");
+                    System.out.println(" *****  ");
+                    System.out.println();
+                break;
+                case 2:
+                    System.out.println("  ***  ");
+                    System.out.println("     * ");
+                    System.out.println("     * ");
+                    System.out.println("  ***  ");
+                    System.out.println(" *     ");
+                    System.out.println(" *     ");
+                    System.out.println("  ***  ");
+                    System.out.println();
+                    break;
+                case 3:
+                    System.out.println("  ***  ");
+                    System.out.println("     * ");
+                    System.out.println("     * ");
+                    System.out.println("  ***  ");
+                    System.out.println("     * ");
+                    System.out.println("     * ");
+                    System.out.println("  ***  ");
+                    System.out.println();
+                    break;
+                case 4:
+                    System.out.println(" *   * ");
+                    System.out.println(" *   * ");
+                    System.out.println(" *   * ");
+                    System.out.println(" ***** ");
+                    System.out.println("     * ");
+                    System.out.println("     * ");
+                    System.out.println("     * ");
+                    System.out.println();
+                    break;
+                case 5:
+                    System.out.println(" ***** ");
+                    System.out.println(" *     ");
+                    System.out.println(" *     ");
+                    System.out.println(" ***** ");
+                    System.out.println("     * ");
+                    System.out.println("     * ");
+                    System.out.println(" ***** ");
+                    System.out.println();
+                    break;
+                case 6:
+                    System.out.println("  ***  ");
+                    System.out.println(" *     ");
+                    System.out.println(" *     ");
+                    System.out.println(" ***** ");
+                    System.out.println(" *   * ");
+                    System.out.println(" *   * ");
+                    System.out.println("  ***  ");
+                    System.out.println();
+                    break;
+                case 7:
+                    System.out.println("****** ");
+                    System.out.println("     * ");
+                    System.out.println("    *  ");
+                    System.out.println("   *   ");
+                    System.out.println("  *    ");
+                    System.out.println(" *     ");
+                    System.out.println("*      ");
+                    System.out.println();
+                    break;
+                case 8:
+                    System.out.println("  ***  ");
+                    System.out.println(" *   * ");
+                    System.out.println(" *   * ");
+                    System.out.println("  ***  ");
+                    System.out.println(" *   * ");
+                    System.out.println(" *   * ");
+                    System.out.println("  ***  ");
+                    System.out.println();
+                    break;
+                case 9:
+                    System.out.println("  ***  ");
+                    System.out.println(" *   * ");
+                    System.out.println(" *   * ");
+                    System.out.println("  **** ");
+                    System.out.println("     * ");
+                    System.out.println("     * ");
+                    System.out.println("  ***  ");
+                    System.out.println();
+                    break;
+            }
+        } 
+
+        stdIn.close();
+    }
+}
